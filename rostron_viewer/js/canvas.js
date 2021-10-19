e88f628393284b4925b1ec70b2be570f67476905
Etienne Schmitz <contact@etienne-schmitz.com>
2021-10-19 20:41:05 +0200

    fix - update and show field

--- rostron_viewer/js/canvas.js
+++ rostron_viewer/js/canvas.js
@@ -2,7 +2,7 @@ const canvas = document.querySelector("#field")
 const ctx = document.querySelector("#field").getContext("2d");
 
 // Canvas variable
-var zoom_mult = 1;
+var zoom_mult = 1.5;
 
 var canvas_size = {
     width: window.innerWidth,
@@ -38,31 +38,31 @@ function update_canvas() {
         y: canvas_size.height / 2 / 100 / zoom_mult
     }
 
-
-    ctx.scale(zoom_mult / 100, -zoom_mult / 100);
     ctx.clearRect(0, 0, canvas_size.width, canvas_size.height);
-    ctx.translate(canvas_center.x, -canvas_center.y);
+    ctx.scale(zoom_mult * 100, zoom_mult * 100);
+    ctx.translate(canvas_center.x, canvas_center.y);
 }
 
 
 function convert_list_to_field(field_list) {
     return {
-        width : field_list[0], 
-        length : field_list[1], 
-        goal_depth : field_list[2], 
-        goal_width : field_list[3], 
-        penalty_width : field_list[4],  
+        width: field_list[0],
+        length: field_list[1],
+        goal_depth: field_list[2],
+        goal_width: field_list[3],
+        penalty_width: field_list[4],
         penalty_depth: field_list[5],
-        boundary_width : field_list[6]
+        boundary_width: field_list[6]
     }
 }
 
 function draw_field(field) {
-    ctx.strokeRect(-field.length / 2, field.width / 2, field.length, field.width)
+    ctx.strokeRect(-field.length / 2, -field.width / 2, field.length, field.width)
+    // ctx.strokeRect(0, 0, 150,150);
 }
 
 window.addEventListener("load", () => {
-    const backend;
+    var backend;
 
     new QWebChannel(qt.webChannelTransport, (channel) => {
         backend = channel.objects.backend
@@ -70,20 +70,20 @@ window.addEventListener("load", () => {
         setInterval(() => {
             backend.get_field().then((field_list) => {
                 const field = convert_list_to_field(field_list);
-                
+
                 update_canvas();
-                ctx.strokeStyle = "#FFF";
-                ctx.lineWidth = field.boundary_width;
-                
+
+                // ctx.fillStyle = "#26a349";
+                // ctx.fillRect(-canvas.width, -canvas.height, canvas.width, canvas.height);
+
+                ctx.strokeStyle = "#fff";
+                ctx.lineWidth = field.boundary_width / 10;
+                console.warn(field.length);
+
                 draw_field(field);
-
-
             });
         }, 60)
     })
 })
 
 
-
-// ctx.fillStyle = "blue";
-// ctx.fillRect(0, 0, canvas.width, canvas.height);
